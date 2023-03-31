a408800ac01b0e2f25866b0dcad18bc6a1f20daf
typing-art <typingart.94@gmail.com>
2023-03-31 16:06:16 +0700

    first commit

---
+++ components/Welcome.js
@@ -0,0 +1,44 @@
+import Mintingcomponent from "./Mintingcomponent"
+import Partnerscomponent from "./Partnerscomponent"
+import { useState, useEffect } from "react"
+import { useNetwork } from "wagmi"
+
+export default function Welcome() {
+    const { chain } = useNetwork()
+
+    const [chainnow, setchainnow] = useState("Goerli")
+    useEffect(() => {
+        if (chain) {
+            if (chain["id"] == 5) {
+                setchainnow(chain["name"])
+            }
+            if (chain["id"] == 80001) {
+                setchainnow(chain["name"])
+            }
+        }
+    }, [chain])
+
+    return (
+        <div className="scroll-smooth">
+            <div>
+                <div className="grid items-center justify-items-center bg-cover bg-gradient-to-r from-indigo-500 via-blue-500 to-sky-400 relative">
+                    <div className="mt-8 grid items-center justify-items-center text-center opacity-100 ">
+                        <h1 className="mt-5 font-Rubik lg:text-8xl md:text-6xl sm:text-4xl font-bold text-white">
+                            DISCLAIMER
+                        </h1>
+                        <h1 className="mt-5 ml-11 mr-11 font-Rubik lg:text-3xl md:text-2xl sm:text-xl text-white">
+                            This is a minting launchpad for the mint NFT test from a collection
+                            devoted to learning material for people new to nft, they can learn the
+                            mint nft process with testnet funds instead of using their real funds.
+                            If this involves your company's copyright issues, please contact me and
+                            I will remove it immediately.
+                        </h1>
+                    </div>
+                    <hr />
+                    <Mintingcomponent />
+                    <p className="mb-10"></p>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+import Bottom from "../components/Bottom"
+import Header from "../components/Header"
+import Welcome from "../components/Welcome"
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>NftDrop Testnet</title>
+                <meta name="description" content="Goerli testnet minting site" />
+                <link rel="icon" href="/Cjdowner-Cryptocurrency-Flat-Ethereum-ETH.ico" />
+            </Head>
+            <Header />
+            <Welcome />
+        </div>
+    )
+}
