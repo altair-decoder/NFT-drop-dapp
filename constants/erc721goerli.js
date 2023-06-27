129b9a30a6e120d73afee2185964acb122634a4c
rzoktan <rz.oktan@gmail.com>
2023-06-27 19:05:04 +0700

    update

--- constants/erc721goerli.js
+++ constants/erc721goerli.js
@@ -6,7 +6,7 @@ export const erc721goerli = [
         name: "FlappyOwl",
         address: "0x53d06c5b6b0e719a03e2665fb9643a2e8c20154b",
         pic: "https://openseauserdata.com/files/8193870c76606a2ad728fbcc99d46768.svg",
-        scan: "https://goerli.etherscan.io/address/",
+        scan: "https://etherscan.io/address/",
         opensea: "flappyowl",
     }
 ]
